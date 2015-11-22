685a1fcdd06daff44f51b00ba47990c28c79d7b2
juniormesquitadandao <juniormesquitadandao@gmail.com>
2015-11-22 15:13:51 -0300

    belongsTo, hasMany

--- app/models/eloquent_js_model.js
+++ app/models/eloquent_js_model.js
@@ -3,12 +3,19 @@
 var EloquentJs = Object.create({
   window: this,
   attrAccessible: function(){
-    
-    var __attrAccessible = Array.prototype.slice.call(arguments) || [];
+    var __attrAccessible = Array.prototype.slice.call(arguments);
     __attrAccessible.forEach(function(_attrAccessible){
       self[_attrAccessible] = null;
     });
-
+  },
+  belongsTo: function(){
+    var __belongsTo = Array.prototype.slice.call(arguments).shift();
+    attrAccessible(__belongsTo);
+  },
+  hasMany: function(){
+    var __hasMany = Array.prototype.slice.call(arguments).shift();
+    attrAccessible(__hasMany);
+    self[__hasMany] = [];
   }
 });
 
@@ -19,10 +26,14 @@ EloquentJs.Model = function(name, body){
   eval.call(EloquentJs.window, "function __name__(){\
       var self = this;\
       var attrAccessible = __attrAccessible__;\
+      var belongsTo = __belongsTo__;\
+      var hasMany = __hasMany__;\
       (__body__)(this);\
     };"
     .replace(/\b__name__\b/, name)
     .replace(/\b__attrAccessible__\b/, EloquentJs.attrAccessible.toString())
+    .replace(/\b__belongsTo__\b/, EloquentJs.belongsTo.toString())
+    .replace(/\b__hasMany__\b/, EloquentJs.hasMany.toString())
     .replace(/\b__body__\b/, body.toString())
   );
   var Model = eval(name);
--- spec/models/eloquent_js_model_spec.js
+++ spec/models/eloquent_js_model_spec.js
@@ -1,33 +1,55 @@
 var expect = require('expect.js');
 var EloquentJs = require('../../app/models/eloquent_js_model.js');
 EloquentJs
-  .Model('User', function(){
-    attrAccessible('email', 'cliente');
-  })
   .Model('Cliente', function(){
     attrAccessible('name', 'phone');
+  })
+  .Model('User', function(){
+    attrAccessible('email');
+    belongsTo('cliente');
+    belongsTo('permission');
+  })
+  .Model('Permission', function(){
+    attrAccessible('name');
+    hasMany('clientes');
+  })
+  .Model('Stub', function(){
   });
 
 describe('EloquentJs', function() {
 
+  it('::models', function () {
+    var modelNames = EloquentJs.models.map(function(model){
+      return model.name;
+    });
+
+    expect(modelNames.join(', ')).to.be('Cliente, User, Permission, Stub');
+  });
+
   describe('Model', function() {
 
-    it('create', function () {
-      expect(User.name).to.be('User');
+    it('::name', function () {
       expect(Cliente.name).to.be('Cliente');
+      expect(User.name).to.be('User');
+      expect(Permission.name).to.be('Permission');
+      expect(Stub.name).to.be('Stub');
     });
 
-    it('save', function () {
-      var modelNames = EloquentJs.models.map(function(model){
-        return model.name;
-      });
-
-      expect(modelNames.join(', ')).to.be('User, Cliente');
-    });
 
     it('#toJson', function () {
-      expect(new User().toJson).to.be('{"email":null,"cliente":null}');
+      expect(new Stub().toJson).to.be('{}');
+    });
+
+    it('attrAccessible', function () {
       expect(new Cliente().toJson).to.be('{"name":null,"phone":null}');
+    });
+
+    it('belongsTo', function () {
+      expect(new User().toJson).to.be('{"email":null,"cliente":null,"permission":null}');
+    });
+
+    it('hasMany', function () {
+      expect(new Permission().toJson).to.be('{"name":null,"clientes":[]}');
     });
 
   });
