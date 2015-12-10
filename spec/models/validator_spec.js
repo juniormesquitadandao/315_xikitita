c7a2da6534ab1ddd43f7d356891d6049843b01df
juniormesquitadandao <juniormesquitadandao@gmail.com>
2015-12-09 22:39:13 -0300

    validatiors

--- spec/models/validator_spec.js
+++ spec/models/validator_spec.js
@@ -5,15 +5,23 @@ describe('Validation', function() {
   before(function() {
     Xikitita
       .init
+      .Validator('custom', function(value, attrName, instance, options){
+        return value;
+      })
   });
 
   it('::validators', function(){
-    expect(Object.keys(Xikitita.validators).toJson).to.be('["presence"]');
+    expect(Object.keys(Xikitita.validators).toJson).to.be('["presence","custom"]');
   });
 
   it('::validators.presence', function(){
     expect(Xikitita.validators.presence(null)).to.be(false);
     expect(Xikitita.validators.presence({})).to.be(true);
+  });
+
+  it('::validators.custom', function(){
+    expect(Xikitita.validators.custom(false)).to.be(false);
+    expect(Xikitita.validators.custom(true)).to.be(true);
   });
 
 });
